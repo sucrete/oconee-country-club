2ccaca020aa8b40426605371419bdd7d8c335e36
Will Andree <williamandree@gmail.com>
2025-12-17 16:52:56 -0600

    Updated Golf Lessons page and data structure feeding Course Layout page.

---
+++ assets/js/data/holeData.js
@@ -0,0 +1,384 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("holesData", () => ({
+    holes: [
+      {
+        hole: "1",
+        par: "4",
+        description:
+          "A tough opener requiring accuracy; fairway bunkers guard the drive and a pond protects the small, sloping green.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "2",
+        par: "4",
+        description:
+          "A challenging hole defined by tree-lined fairways leading to a small, elevated green that slopes severely from back to front.",
+        pictureURL: "images/hole-2.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "394",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "376",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "3",
+        par: "4",
+        description:
+          "This shorter dogleg left demands a precise lay-up to avoid the fairway bunker on the right for the best angle into the green.",
+        pictureURL: "images/hole-3.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "358",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "338",
+          },
+          {
+            teeBox: "red",
+            yardage: "285",
+          },
+        ],
+      },
+      {
+        hole: "4",
+        par: "4",
+        description:
+          "A tough opening hole requiring an accurate tee shot to the right-center fairway; approach shot must avoid the small, undulating green's large bunkers.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "5",
+        par: "4",
+        description:
+          "A tough opening hole requiring an accurate tee shot to the right-center fairway; approach shot must avoid the small, undulating green's large bunkers.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "6",
+        par: "4",
+        description:
+          "A tough opening hole requiring an accurate tee shot to the right-center fairway; approach shot must avoid the small, undulating green's large bunkers.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "7",
+        par: "4",
+        description:
+          "A tough opening hole requiring an accurate tee shot to the right-center fairway; approach shot must avoid the small, undulating green's large bunkers.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "8",
+        par: "4",
+        description:
+          "A tough opening hole requiring an accurate tee shot to the right-center fairway; approach shot must avoid the small, undulating green's large bunkers.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "9",
+        par: "4",
+        description:
+          "A tough opening hole requiring an accurate tee shot to the right-center fairway; approach shot must avoid the small, undulating green's large bunkers.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "10",
+        par: "4",
+        description:
+          "A tough opening hole requiring an accurate tee shot to the right-center fairway; approach shot must avoid the small, undulating green's large bunkers.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "11",
+        par: "4",
+        description:
+          "A tough opening hole requiring an accurate tee shot to the right-center fairway; approach shot must avoid the small, undulating green's large bunkers.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "12",
+        par: "4",
+        description:
+          "A tough opening hole requiring an accurate tee shot to the right-center fairway; approach shot must avoid the small, undulating green's large bunkers.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "13",
+        par: "4",
+        description:
+          "A tough opening hole requiring an accurate tee shot to the right-center fairway; approach shot must avoid the small, undulating green's large bunkers.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "14",
+        par: "4",
+        description:
+          "A tough opening hole requiring an accurate tee shot to the right-center fairway; approach shot must avoid the small, undulating green's large bunkers.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "15",
+        par: "4",
+        description:
+          "A tough opening hole requiring an accurate tee shot to the right-center fairway; approach shot must avoid the small, undulating green's large bunkers.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "16",
+        par: "4",
+        description:
+          "A tough opening hole requiring an accurate tee shot to the right-center fairway; approach shot must avoid the small, undulating green's large bunkers.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "17",
+        par: "4",
+        description:
+          "A tough opening hole requiring an accurate tee shot to the right-center fairway; approach shot must avoid the small, undulating green's large bunkers.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+      {
+        hole: "18",
+        par: "4",
+        description:
+          "A tough opening hole requiring an accurate tee shot to the right-center fairway; approach shot must avoid the small, undulating green's large bunkers.",
+        pictureURL: "images/hole-1.jpg",
+        teeBoxes: [
+          {
+            teeBox: "blue",
+            yardage: "356",
+          },
+          {
+            teeBox: "yellow",
+            yardage: "348",
+          },
+          {
+            teeBox: "red",
+            yardage: "332",
+          },
+        ],
+      },
+    ],
+  }));
+});
